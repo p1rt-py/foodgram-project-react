9bcabcda3dfd79edd1f62ae631dd00c7aa82bdb4
Altynov Ilia <103097530+p1rt-py@users.noreply.github.com>
2022-12-09 13:58:13 +0300

    'permissions up'

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -4,12 +4,16 @@ from rest_framework.permissions import (SAFE_METHODS, BasePermission,
 
 class AdminOrReadOnly(BasePermission):
     def has_permission(self, request, obj):
-        return (request.method in SAFE_METHODS
-                or request.user.is_staff)
+        return (
+                request.method in SAFE_METHODS
+                or request.user.is_staff
+        )
 
 
 class IsOwnerOrReadOnly(IsAuthenticatedOrReadOnly):
     def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS
+        return (
+                request.method in SAFE_METHODS
                 or (request.user == obj.author)
-                or request.user.is_staff)
+                or request.user.is_staff
+        )
