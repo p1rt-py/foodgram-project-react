e744fcd4d9926bc3126af7c18fee5da5ea418ed0
Altynov Ilia <103097530+p1rt-py@users.noreply.github.com>
2022-12-09 14:27:38 +0300

    'filters up'

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,5 @@
 from django_filters import AllValuesMultipleFilter
-from django_filters.rest_framework import filters
+from django_filters import rest_framework as filters
 from django_filters.widgets import BooleanWidget
 from recipes.models import Recipe
 # from rest_framework.filters import SearchFilter
