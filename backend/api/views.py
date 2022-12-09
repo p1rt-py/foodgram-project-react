26c8219d084a59071fce5ce53483e855f1d7706f
Altynov Ilia <103097530+p1rt-py@users.noreply.github.com>
2022-12-09 15:17:49 +0300

    'download cart'

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.db.models import BooleanField, Exists, OuterRef, Value
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 from recipes.models import Cart, Favorite, Ingredient, Recipe, Tag
@@ -8,7 +7,7 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
-
+from .utils import download_cart
 from .filters import IngredientFilter, RecipeFilter
 from .pagination import LimitPageNumberPagination
 from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
@@ -158,35 +157,39 @@ class RecipeViewSet(viewsets.ModelViewSet):
             'errors': 'Ошибка удаления рецепта из списка'
         }, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(
-        detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    @action(detail=False, permission_classes=[IsAuthenticated])
     def download_shopping_cart(self, request):
-        user = get_object_or_404(User, username=request.user.username)
-        shopping_cart = user.cart.all()
-        shopping_dict = {}
-        for num in shopping_cart:
-            ingredients_queryset = num.recipe.ingredient.all()
-            for ingredient in ingredients_queryset:
-                name = ingredient.ingredients.name
-                amount = ingredient.amount
-                measurement_unit = ingredient.ingredients.measurement_unit
-                if name not in shopping_dict:
-                    shopping_dict[name] = {
-                        'measurement_unit': measurement_unit,
-                        'amount': amount}
-                else:
-                    shopping_dict[name]['amount'] = (
-                        shopping_dict[name]['amount'] + amount)
+        return download_cart(request)
 
-        shopping_list = []
-        for index, key in enumerate(shopping_dict, start=1):
-            shopping_list.append(
-                f'{index}. {key} - {shopping_dict[key]["amount"]} '
-                f'{shopping_dict[key]["measurement_unit"]}\n')
-        filename = 'shopping_cart.txt'
-        response = HttpResponse(shopping_list, content_type='text/plain')
-        response['Content-Disposition'] = f'attachment; filename={filename}'
-        return response
+    # @action(
+    #     detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    # def download_shopping_cart(self, request):
+    #     user = get_object_or_404(User, username=request.user.username)
+    #     shopping_cart = user.cart.all()
+    #     shopping_dict = {}
+    #     for num in shopping_cart:
+    #         ingredients_queryset = num.recipe.ingredient.all()
+    #         for ingredient in ingredients_queryset:
+    #             name = ingredient.ingredients.name
+    #             amount = ingredient.amount
+    #             measurement_unit = ingredient.ingredients.measurement_unit
+    #             if name not in shopping_dict:
+    #                 shopping_dict[name] = {
+    #                     'measurement_unit': measurement_unit,
+    #                     'amount': amount}
+    #             else:
+    #                 shopping_dict[name]['amount'] = (
+    #                     shopping_dict[name]['amount'] + amount)
+    #
+    #     shopping_list = []
+    #     for index, key in enumerate(shopping_dict, start=1):
+    #         shopping_list.append(
+    #             f'{index}. {key} - {shopping_dict[key]["amount"]} '
+    #             f'{shopping_dict[key]["measurement_unit"]}\n')
+    #     filename = 'shopping_cart.txt'
+    #     response = HttpResponse(shopping_list, content_type='text/plain')
+    #     response['Content-Disposition'] = f'attachment; filename={filename}'
+    #     return response
 
 # from django.contrib.auth import get_user_model
 # from django.db.models import BooleanField, Exists, OuterRef, Value
