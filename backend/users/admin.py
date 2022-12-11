5bae8f887251135ee5298ccf487f4b614824ebdc
Altynov Ilia <103097530+p1rt-py@users.noreply.github.com>
2022-12-11 16:43:20 +0300

    'test'

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.admin import register
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-
 from .models import Follow
 
 
